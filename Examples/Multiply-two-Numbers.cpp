b40a131d81acd66f9ff8641543bacead1cc06687
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-03-30 23:18:14 +0530

    Program to Multiply two Numbers

---
+++ Examples/Multiply-two-Numbers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	float n1, n2, result;
+	cout << "Enter two numbers: ";
+	cin >> n1 >> n2;
+
+	cout << "Product = " << n1*n2;
+	return 0;
+}
