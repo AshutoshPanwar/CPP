5cd10b1b4736d6e27f4c71176d2422d33c918418
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-04-04 17:44:14 +0530

    The program multiplies these two matrices (if possible) and displays it on the screen.

---
+++ Examples/Multiply-Two-Matrix-Using-Multi-dimensional-Arrays.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int row, col, arr1[100][100], arr2[100][100], mul[100][100];
+	cout << "Enter number of rows (between 1 and 100): ";
+	cin >> row;
+	cout << "Enter number of columns (between 1 and 100): ";
+	cin >> col;
+
+	cout << "Enter elements of 1st matrix:" << endl;
+	for (int i = 0; i < row; ++i)
+	{
+		for (int j = 0; j < col; ++j)
+		{
+			cout << "Enter element of a" << i+1<< j+1 << " = " ;
+			cin >> arr1[i][j];
+		}
+	}
+	cout << "Enter elements of 2st matrix:" << endl;
+	for (int i = 0; i < row; ++i)
+	{
+		for (int j = 0; j < col; ++j)
+		{
+			cout << "Enter element of b" << i+1<< j+1 << " = " ;
+			cin >> arr2[i][j];
+		}
+	}
+
+	for (int i = 0; i < row; ++i)
+	{
+		for (int j = 0; j < col; ++j)
+		{
+			mul[i][j] = arr1[i][j] * arr2[i][j];
+		}
+	}
+
+	cout << endl << "Product of two matrix is: " << endl;
+	for (int i = 0; i < row; ++i)
+	{
+		for (int j = 0; j < col; ++j)
+		{
+			
+			cout << mul[i][j] << " ";
+			if (j == col-1)
+			{
+				cout << endl;
+			}
+		}
+	}
+
+
+	return 0;
+}
