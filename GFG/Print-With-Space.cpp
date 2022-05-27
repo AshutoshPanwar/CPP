42222df6a75d52d593dea47d6bfaac2069d6613a
Ashutosh Panwar <81963021+AshutoshPanwar@users.noreply.github.com>
2022-05-28 01:16:36 +0530

    Printing spcaes

---
+++ GFG/Print-With-Space.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string a = "Hello", b = "World";
+
+    cout << a << " " << b << endl;      // Printing blank space b/w two strings
+    return 0;
+}
