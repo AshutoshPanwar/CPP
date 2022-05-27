3df1a48fe80c4800bc2a3a278e852e584320657f
Ashutosh Panwar <81963021+AshutoshPanwar@users.noreply.github.com>
2022-05-28 00:34:33 +0530

    Number form 1 to kth element

---
+++ GFG/Kth-call.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cin >> num;
+
+    // Print number form 1 to num
+    for (int i = 1; i <= num; i++)
+    {
+        cout << i << " ";
+    }
+    return 0;
+}
