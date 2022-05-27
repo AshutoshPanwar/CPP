71b4b2f1fe06f82c45eb78b1cd440929cde78e89
Ashutosh Panwar <81963021+AshutoshPanwar@users.noreply.github.com>
2022-05-27 23:23:46 +0530

    Single line and Multi-line comments

---
+++ GFG/Learn-to-Comment.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    /* This a
+    Multi-line comment */
+
+    int num1=5, num2=10;
+    cout << num1+num2;      // This is a single line comment
+    return 0;
+}
