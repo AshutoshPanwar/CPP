952e707124f6285818c950495886492116acf013
Ashutosh Panwar <81963021+AshutoshPanwar@users.noreply.github.com>
2022-06-09 22:18:46 +0530

    check if the given number n is prime or not using for loop

---
+++ GFG/For-Loop-primeCheck.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+bool isPrime(int n)
+{
+    if (n == 1 || n == 0)
+    {
+        return false;
+    }
+    for (int i = 2; i < n; i++)
+    {
+        if (n % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main()
+{
+    int num;
+    cin >> num;
+    string res = isPrime(num) ? "Yes": "No";
+    cout << boolalpha;
+    cout << res;
+    return 0;
+}
