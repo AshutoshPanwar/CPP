c426e741f72ee0a5df4cfcde0fb02e9745098477
Ashutosh Panwar <81963021+AshutoshPanwar@users.noreply.github.com>
2022-06-28 23:14:45 +0530

    Transpose of a matrix

---
+++ GFG/Transpose-of-Matrix.cpp
@@ -0,0 +1,47 @@
+// TransPose of a matrix of size n
+// Size of matrix must be N*N
+// Interchange the row and column
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter the matrix size: ";
+    cin >> n;
+    cout << endl;
+
+
+    int arr[n][n];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> arr[i][j];
+        }
+        cout << endl;
+    }
+
+
+    cout << "Original Matrix:" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+
+    cout << endl << "Transpose of Matrix: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << arr[j][i] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
